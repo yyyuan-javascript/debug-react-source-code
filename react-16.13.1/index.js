fcc68e0e7b19d6c6f9e4f9a17bfeee6e15c42ed2
yuanyuan_yang <yuanyuan_yang@trip.com>
2020-07-21 08:29:28 +0800

    feat: add app component

--- react-16.13.1/index.js
+++ react-16.13.1/index.js
@@ -1,5 +1,21 @@
+const {useState, useEffect} = React;
+
 function App() {
-    return <div>Hello, React Source Code!</div>
+const [count, setCount] = useState(0);
+const handleScroll = () => {
+    setCount(count+1);
+};
+useEffect(()=>{
+    window.addEventListener('scroll',handleScroll);
+return ()=>{
+    window.removeEventListener('scroll',handleScroll);
+}
+},[count]);
+return <div className="wrapper">
+    <div className="inner">
+    {`It's the ${count} times to trigger scroll.`}
+    </div>
+</div>
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'))
+ReactDOM.render(<App/>, document.querySelector('#app'));
